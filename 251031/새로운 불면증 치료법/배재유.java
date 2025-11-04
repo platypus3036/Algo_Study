2d37bfbc3cb71241f33d46088177e11ac6eb7852
winter3671 <wjdgns9805@naver.com>
2025-11-04 13:36:29 +0900

    Update 배재유.java

--- 251031/새로운 불면증 치료법/배재유.java
+++ 251031/새로운 불면증 치료법/배재유.java
@@ -1,211 +1,70 @@
-
 import java.util.*;
 import java.io.*;
-
+ 
 class Solution
 {
-	
-	static int H;
-	static int W;
- 	public static void main(String args[]) throws Exception
-	{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-
-		/*
-		 * 
-		 * 명령어 이후 맵의 상태
-		 * 
-		 * 시뮬레이션
-		 * 
-		 * 전차는 어짜피 1대
-		 * 매번 전차 위치 확인하여 문제 처리
-		 * 
-		 * 
-		 * */
-		
-		int T = Integer.parseInt(br.readLine());
-		for(int tc =1; tc<=T; tc++) {
-			StringTokenizer st = new StringTokenizer (br.readLine());
-			H = Integer.parseInt(st.nextToken());
-			W = Integer.parseInt(st.nextToken());
-			char[][] map = new char[H][W];
-			for(int i = 0; i<H; i++) {
-				map[i] = br.readLine().toCharArray();
-			}
-			int command_len = Integer.parseInt(br.readLine());
-			//시뮬레이션 시작 전 위치 찾기
-			int sy = -1;
-			int sx = -1;
-			int dir = -1;
-			//방향은 상 부터 시계방향으로 0123
-			for(int i = 0; i<H; i++) {
-				for(int j=0; j<W; j++) {
-					if(map[i][j] == '^') {
-						sy = i;
-						sx = j;
-						dir = 0;
-						break;
-					} else if(map[i][j] == '>')  {
-						sy = i;
-						sx = j;
-						dir = 1;
-						break;
-					} else if(map[i][j] == 'v') {
-						sy = i;
-						sx = j;
-						dir = 2;
-						break;
-					} else if(map[i][j] == '<') {
-						sy = i;
-						sx = j;
-						dir = 3;
-						break;
-					}
-				}
-				
-			}
-			
-			String command = br.readLine();
-			for(int i = 0; i<command_len; i++) {
-				if(command.charAt(i) == 'S') {
-					if(dir == 0) {
-						//sy에서 0까지 탐색
-						for(int j = sy-1; j>-1; j--) {
-							if(map[j][sx] == '#') {
-								break;
-							} else if(map[j][sx] == '*') {
-								map[j][sx] = '.';
-								break;
-							} 
-						}
-					} else if (dir ==1) {
-						//우측 sx에서 W까지
-						for(int j = sx+1; j<W; j++) {
-							if(map[sy][j] == '#') {
-								break;
-							} else if(map[sy][j] == '*') {
-								map[sy][j] = '.';
-								break;
-							} 
-						}
-					} else if (dir==2) {
-						//하단 sy에서 H까지
-						for(int j = sy+1; j<H; j++) {
-							if(map[j][sx] == '#') {
-								break;
-							} else if(map[j][sx] == '*') {
-								map[j][sx] = '.';
-								break;
-							} 
-						}
-					} else {
-						//좌측 sx부터 0까지
-						for(int j = sx-1; j>-1; j--) {
-							if(map[sy][j] == '#') {
-								break;
-							} else if(map[sy][j] == '*') {
-								map[sy][j] = '.';
-								break;
-							} 
-						}
-					}
-				} else if (command.charAt(i) == 'U') {
-					//방향 위로, 평지면 이동, 이동 전 범위 확인
-					//방향은 이동 불가와 상관없이 변경
-					//방향 변경에 대해서도 탱크 표시도 기본적으로 변경해야한다
-					dir = 0;
-					map[sy][sx] = '^';
-					int ny = sy-1;
-					int nx = sx;
-					if(isValid(ny,nx) && map[ny][nx] == '.') {
-						char temp = map[sy][sx];
-						map[sy][sx] = '.';
-						map[ny][nx] = temp;
-						//이동 후 좌표 변경
-						sy = ny;
-						sx = nx;
-					}
-				} else if (command.charAt(i) == 'R') {
-					//방향 우측, 평지면 이동, 이동 전 범위 확인
-					//방향은 이동 불가와 상관없이 변경
-					dir = 1;
-					map[sy][sx] ='>';
-					int ny = sy;
-					int nx = sx+1;
-					if(isValid(ny,nx) && map[ny][nx] == '.') {
-						char temp = map[sy][sx];
-						map[sy][sx] = '.';
-						map[ny][nx] = temp;
-						//이동 후 좌표 변경
-						sy = ny;
-						sx = nx;
-					}
-				} else if (command.charAt(i) == 'D') {
-					//방향 아래, 평지면 이동, 이동 전 범위 확인
-					//방향은 이동 불가와 상관없이 변경
-					dir = 2;
-					map[sy][sx] ='v';
-					int ny = sy+1;
-					int nx = sx;
-					if(isValid(ny,nx) && map[ny][nx] == '.') {
-						char temp = map[sy][sx];
-						map[sy][sx] = '.';
-						map[ny][nx] = temp;
-						//이동 후 좌표 변경
-						sy = ny;
-						sx = nx;
-					}
-				} else if (command.charAt(i) == 'L') {
-					//방향 좌측, 평지면 이동, 이동 전 범위 확인
-					//방향은 이동 불가와 상관없이 변경
-					dir = 3;
-					map[sy][sx] ='<';
-					int ny = sy;
-					int nx = sx-1;
-					if(isValid(ny,nx) && map[ny][nx] == '.') {
-						char temp = map[sy][sx];
-						map[sy][sx] = '.';
-						map[ny][nx] = temp;
-						//이동 후 좌표 변경
-						sy = ny;
-						sx = nx;
-					}
-				} 
-			}
-			//출력
-			//첫 번째 줄은 tc와 함께 출력
-			String temp = String.format("#%d ", tc);
-			for(int i = 0 ; i<W; i++) {
-				temp+=String.valueOf(map[0][i]);
-			}
-			bw.write(temp+"\n");
-			for(int i = 1; i<H; i++) {
-				StringBuilder sb = new StringBuilder();
-				for(int j =0; j<W; j++) {
-					sb.append(map[i][j]);
-				}
-				bw.write(sb.toString()+"\n");
-			}
-			
-			
-		}
-		
-
-		
-
-		
-		
-		
-
-
-		bw.flush();
-		bw.close();
-			
-	}
- 	
- 	public static boolean isValid(int y, int x) {
- 		return 0<=x && x<W && 0<=y && y<H;
- 	}
+     
+    static char[][] map;
+    public static void main(String args[]) throws Exception
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+         
+ 
+        /*
+         * 
+         * 최소 몇 번 양을 세었을 때 이전에 봤던 숫자들의 자릿수에서 0에서 9까지 모든 숫자 보게 되는지
+         * 
+         * 0부터 9까지 모든 숫자를 보게 되는지 출력
+         * 
+         * set으로 확인
+         * 
+         * 그냥 검사 종료될 때 종료
+         * 
+         * 
+         * 
+         * */
+         
+        int T = Integer.parseInt(br.readLine());
+        for(int tc=1; tc<= T; tc++) {
+            int N = Integer.parseInt(br.readLine());
+            Set<Character> set = new HashSet<>();
+//          int cnt = 1;
+            int num = N;
+            while(true) {
+                //N을 쪼개어
+                String str = num+"";
+                for(int i = 0; i<str.length(); i++) {
+                    set.add(str.charAt(i));
+                }
+                if(check_num(set)) {
+                    break;
+                }
+                //2의 배수가 아니라 N의 배수
+                num+=N;
+            }
+            //출력도 현재  num
+            bw.write(String.format("#%d %d\n",tc,num));
+        }
+ 
+         
+ 
+         
+         
+         
+ 
+ 
+        bw.flush();
+        bw.close();
+             
+    }
+     
+    public static boolean check_num(Set<Character> set) {
+//      System.out.println(set);
+ 
+        return set.size() == 10;
+    }
+     
+ 
+     
 }
